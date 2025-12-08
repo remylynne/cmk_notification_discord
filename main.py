a5a2382bbb6786d2a1eb61c44cff61009b2b821e
Remy Lynne <remy@lynne.dev>
2025-12-08 10:34:16 +0100

    script does not start itself

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
-#!/usr/bin/env python3
-# Discord Webhook
-
 from .scripts.discord_client import buildDiscordMessage, sendDiscordMessage
 from .scripts.notification_mapper import getNotification
 
 
 
-if __name__ == "__main__":
+def main():
     notification = getNotification()
     message = buildDiscordMessage(notification)
     sendDiscordMessage(message)
